9f865a46044b0db2f4c9595ee21874931355dc69
Widiyanto <widy.octa@gmail.com>
2017-02-15 23:06:10 +0700

    Add files via upload

---
+++ wot_resource_server/src/test/java/wot/resource/server/WotResourceServerApplicationTests.java
@@ -0,0 +1,16 @@
+package wot.resource.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WotResourceServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
